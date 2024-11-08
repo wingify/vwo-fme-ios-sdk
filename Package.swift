70e1f59b92bfe0327aa24f6d0429ecc413c91fa6
vishwajeet-wingify <vishwajeet.singh@vwo.com>
2024-11-08 15:53:06 +0530

    chore: Package file for SPM support

---
+++ Package.swift
@@ -0,0 +1,35 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "VWO-FME",
+    platforms: [
+        .iOS(.v12)
+    ],
+    products: [
+        .library(
+            name: "VWO-FME",
+            targets: ["VWO-FME"]),
+    ],
+    dependencies: [
+        // Add any dependencies here, if needed.
+    ],
+    targets: [
+        .target(
+            name: "VWO-FME",
+            dependencies: [],
+            path: "VWO-FME",
+            exclude: [],
+            resources: [
+                .process("Resources")
+            ],
+            publicHeadersPath: ".",
+            cSettings: [
+                .headerSearchPath("VWO-FME")
+            ]
+        )
+    ],
+    swiftLanguageVersions: [.v5]
+)
